23250486362be1deccf673739b27beca347e1378
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-04-10 17:44:03 +0800

    Сделать 2 экрана: логин и приветствие

--- appWithTwoViews/LoginViewController.swift
+++ appWithTwoViews/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  appWithTwoViews
 //
 //  Created by Александр on 10.04.2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
 }
-
---
+++ appWithTwoViews/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  appWithTwoViews
+//
+//  Created by Александр on 10.04.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
