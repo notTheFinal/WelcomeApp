e7bd2a5c5d87e1819e37aabf6eaa717e5c89ddf0
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-04-23 16:13:22 +0800

    Выводить информацию на achievementsVC

--- appWithTwoViews/ViewControllers/AchievementsViewController.swift
+++ appWithTwoViews/ViewControllers/AchievementsViewController.swift
@@ -11,12 +11,31 @@ class AchievementsViewController: UIViewController {
 
     var achievementsList: [Achievement]!
     
+    @IBOutlet weak var achievementsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        showAchievements()
     }
     
+    private func showAchievements() {
+        for achievement in achievementsList {
+            achievementsLabel.text?.append("\(returnEmoji(achievement.place))\(achievement.year)\n")
+            achievementsLabel.text?.append("- \(achievement.title)\n")
+        }
+    }
 
-
+    private func returnEmoji(_ place: Int?) -> String {
+        switch place {
+        case 1:
+            return "🥇"
+        case 2:
+            return "🥈"
+        case 3:
+            return "🥉"
+        default:
+            return "🎗"
+        }
+    }
 }
